ac2b92eeffa458711d9670eb23a7f4910e2e25d3
neftona <isc_nefgarcia@hotmail.com>
2015-06-23 12:27:53 -0500

    proyect grid
    Proyectos

--- php/admin/pages/projects_grid.php
+++ php/admin/pages/projects_grid.php
@@ -44,7 +44,7 @@ $renglones_proyectos = mysqli_query($esta, $sql);
                         <form class="form-horizontal" role="form">
                             <div class="widget widget-none bg-gray innerAll half margin-slim">
                                 <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Progress:</label>
+                                    <label class="col-sm-2 control-label text-left">Progreso:</label>
                                     <div class="col-md-4 col-sm-6 col-xs-10 innerT half">
                                         <div class="progress progress-mini">
 											<?php 
@@ -75,22 +75,25 @@ $renglones_proyectos = mysqli_query($esta, $sql);
                             </div>
                             <div class="widget widget-none bg-gray innerAll half margin-slim">
                                 <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Payment:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <p class="lead margin-none">&euro;2500 <span class="text-small text-muted-dark strong">(deposit)</span> </p>
+                                    <label class="col-sm-2 control-label text-left">Fecha:</label>
+                                    <div class="col-sm-10">
+                                        <p class="lead margin-none"><?= $renglon_proyecto['fecha_inicio']." - ".$renglon_proyecto['fecha_entrega']?><span class="text-small text-muted-dark strong">(inicio-entrega)</span> </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="widget widget-none bg-gray innerAll half margin-slim">
                                 <div class="row">
                                     <label class="col-sm-2 control-label text-left innerT">Assigned:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
+                                    <div class="col-md-10 col-sm-6 col-xs-10">
+									<?php 
+										$query2 = "select * from proyectos_usuarios pu JOIN usuarios u on pu.id_usuario_fk=u.id where pu.id_proyecto_fk = ".$renglon_proyecto['id'];
+										$users = mysqli_query($esta, $query2);
+										while($user = mysqli_fetch_assoc($users)){
+									?> 
                                         <a href="">
-                                            <img src="../assets/images/people/50/2.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Adrian Demian">
+                                            <img src="imagenes/<?= $user['imagen'] ?>" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="<?= $user['nombre'] ?>">
                                         </a>
-                                        <a href="">
-                                            <img src="../assets/images/people/50/12.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Laza Bogdan">
-                                        </a>
+										<?php } ?>
                                     </div>
                                 </div>
                             </div>
@@ -99,180 +102,6 @@ $renglones_proyectos = mysqli_query($esta, $sql);
                 </div>
             </div>
 			<?php } ?>
-            <div class="col-md-6">
-                <div class="widget widget-success widget-small">
-                    <div class="innerAll">
-                        <div class="media">
-                            <div class="pull-left innerR half"> <i class="icon-tv fa-4x icon-faded"></i> </div>
-                            <div class="media-body ">
-                                <h4>
-                                    <a href="index.php?page=project_milestones" class="media-heading">User Interface Design</a>
-                                </h4>
-                                <p class="margin-none text-muted">Short description of the project</p>
-                            </div>
-                        </div>
-                        <form class="form-horizontal" role="form">
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Progress:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 innerT half">
-                                        <div class="progress progress-mini">
-                                            <div class="progress-bar progress-bar-success" style="width: 60%;"></div>
-                                        </div>
-                                    </div>
-                                    <label class="control-label text-left strong text-muted-dark">60 &#37;</label>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left padding-top-none">Tags:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 strong">
-                                        <a href=""><span class="label label-primary">HTML</span></a>
-                                        <a href=""><span class="label label-warning"> CSS</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Payment:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <p class="lead margin-none">&euro;2500 <span class="text-small text-muted-dark strong">(deposit)</span> </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left innerT">Assigned:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <a href="">
-                                            <img src="../assets/images/people/50/2.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Adrian Demian">
-                                        </a>
-                                        <a href="">
-                                            <img src="../assets/images/people/50/12.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Laza Bogdan">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="widget widget-info widget-small">
-                    <div class="innerAll">
-                        <div class="media">
-                            <div class="pull-left innerR half"> <i class="icon-tv fa-4x icon-faded"></i> </div>
-                            <div class="media-body ">
-                                <h4>
-                                    <a href="index.php?page=project_milestones" class="media-heading">AJAX Version</a>
-                                </h4>
-                                <p class="margin-none text-muted">Short description of the project</p>
-                            </div>
-                        </div>
-                        <form class="form-horizontal" role="form">
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Progress:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 innerT half">
-                                        <div class="progress progress-mini">
-                                            <div class="progress-bar progress-bar-info" style="width: 34%;"></div>
-                                        </div>
-                                    </div>
-                                    <label class="control-label text-left strong text-muted-dark">34 &#37;</label>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left padding-top-none">Tags:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 strong">
-                                        <a href=""><span class="label label-primary">HTML</span></a>
-                                        <a href=""><span class="label label-warning"> CSS</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Payment:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <p class="lead margin-none">&euro;2500 <span class="text-small text-muted-dark strong">(deposit)</span> </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left innerT">Assigned:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <a href="">
-                                            <img src="../assets/images/people/50/2.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Adrian Demian">
-                                        </a>
-                                        <a href="">
-                                            <img src="../assets/images/people/50/12.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Laza Bogdan">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="widget widget-inverse widget-small">
-                    <div class="innerAll">
-                        <div class="media">
-                            <div class="pull-left innerR half"> <i class="icon-tv fa-4x icon-faded"></i> </div>
-                            <div class="media-body ">
-                                <h4>
-                                    <a href="index.php?page=project_milestones" class="media-heading">Testing &amp; Launch</a>
-                                </h4>
-                                <p class="margin-none text-muted">Short description of the project</p>
-                            </div>
-                        </div>
-                        <form class="form-horizontal" role="form">
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Progress:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 innerT half">
-                                        <div class="progress progress-mini">
-                                            <div class="progress-bar progress-bar-inverse" style="width: 45%;"></div>
-                                        </div>
-                                    </div>
-                                    <label class="control-label text-left strong text-muted-dark">45 &#37;</label>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left padding-top-none">Tags:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10 strong">
-                                        <a href=""><span class="label label-primary">HTML</span></a>
-                                        <a href=""><span class="label label-warning"> CSS</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left">Payment:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <p class="lead margin-none">&euro;2500 <span class="text-small text-muted-dark strong">(deposit)</span> </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="widget widget-none bg-gray innerAll half margin-slim">
-                                <div class="row">
-                                    <label class="col-sm-2 control-label text-left innerT">Assigned:</label>
-                                    <div class="col-md-4 col-sm-6 col-xs-10">
-                                        <a href="">
-                                            <img src="../assets/images/people/50/2.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Adrian Demian">
-                                        </a>
-                                        <a href="">
-                                            <img src="../assets/images/people/50/12.jpg" alt="photo" class="img-circle" style="width:40px;" data-toggle="tooltip" data-placement="top" data-title="Laza Bogdan">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     <ul class="pagination pagination-centered margin-none">
