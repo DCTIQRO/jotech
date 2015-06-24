7908cb7ac2d5b820ba92fe0f9f0d89b008336b02
neftona <isc_nefgarcia@hotmail.com>
2015-06-24 11:57:40 -0500

    cliente
    version 3

--- php/admin/pages/cliente/permisos.php
+++ php/admin/pages/cliente/permisos.php
@@ -1,3 +1,5 @@
+<?php require_once( getcwd().'/pages/esta.php'); ?>
+<link rel="stylesheet" type="text/css" href="../assets/plugins/AnimatedCheckboxes/css/component.css" />
 <div class="widget-head bg-background text-center">
     <ul>
         <li>
@@ -14,7 +16,7 @@
         </li>
 		<li>
             <a class="glyphicons user" href="?page=cliente&amp;section=contactos&id=<?php echo $_GET['id']; ?>"> <i></i>Contactos</a>
-        </li>
+        </li>	
 		<li class="active">
             <a class="glyphicons lock" href="?page=cliente&amp;section=permisos&id=<?php echo $_GET['id']; ?>"> <i></i>Permisos</a>
         </li>
@@ -23,5 +25,15 @@
 
 <div class="widget-body">
     <div class="row">
+		<form >
+			<?php 
+				//$sql = "select * from proyectos order by fecha_insercion desc";
+				//$renglones_proyectos = mysqli_query($esta, $sql);
+			?>
+			<div class="col-sm-3 ">Nombre</div>
+			<div class="col-sm-2 "><label class="radio"><input id="ver" name="permisos" type="radio"><span class="outer"><span class="inner"></span></span>Ver</label></div>
+			<div class="col-sm-4 "><label class="radio"><input id="modificar" name="permisos" type="radio"><span class="outer"><span class="inner"></span></span>Ver/Modificar</label></div>
+			<div class="col-sm-3 "><label class="radio"><input id="ninguno" name="permisos" type="radio"><span class="outer"><span class="inner"></span></span>Ninguno</label></div>
+		</form>
 	</div>
 </div>
