501f34822efa8adc37caff1586cd691e9c4daf86
neftona <isc_nefgarcia@hotmail.com>
2015-06-25 14:33:26 -0500

    proyectos tareas
    datatables de proyectos y tareas cliente

--- php/admin/pages/clientes.php
+++ php/admin/pages/clientes.php
@@ -1,19 +1,10 @@
 <?php
-
 require_once('esta.php');
-
 extract($_POST);
-
-
-
 $sql = "select * from clientes";
 $renglones_clientes = mysqli_query($esta, $sql);
-
-    
-  
-
-
 ?>
+<link href='../assets/plugins/tables_datatables/css/dataTables.bootstrap.css' rel='stylesheet' type='text/css'>
 <div class="innerLR">
     <h1 class="pull-left">
         Clientes &nbsp; 
@@ -27,45 +18,66 @@ $renglones_clientes = mysqli_query($esta, $sql);
     </div>
     <div class="clearfix"></div>
     <div class="innerTB">
-        <?php while($renglon_cliente = mysqli_fetch_assoc($renglones_clientes)){?>
-        <div class="widget widget-success widget-small ">
-            <div class="widget-body padding-none">
-                <div class=" media innerAll overflow-visible margin-none">
-                    <div class="pull-left innerR half hidden-xs"> <i class="icon-tv-play fa-4x icon-faded"></i> </div>
-                    
-                    <div class="media-body">
-                        <h4>
-                            <a href="index.php?page=cliente&id=<?php echo $renglon_cliente['id']; ?>" class="media-heading"><?php echo $renglon_cliente['nombre']; ?></a>
-                        </h4>
-                        <p class="text-muted"><?php echo $renglon_cliente['website']; ?></p>
-                        <div class="clearfix"></div>
-                        
-                        <div class="widget widget-none innerR innerB half margin-slim pull-left">
-                            <span class="strong"> Etiquetas:</span> 
-                            <span class="strong">
-                                <a href="#"><span class="label label-primary">HTML</span></a>
-                                <a href="#"><span class="label label-warning"> CSS</span></a>
-                            </span>
-                        </div>
-                        <div class="widget widget-none innerR innerB half margin-slim pull-left">
-                            <span class="pull-left strong">Estatus:</span> 
-                            <div class="progress progress-mini set-width">
-                                <div class="progress-bar progress-bar-primary" style="width: 50%;"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php } ?>
+		<div class="table-responsive">
+        <!-- Table -->
+
+			<table id="clientes" class="table table-vcenter table-condensed table-bordered dataTable no-footer table-striped">
+
+				<!-- Table heading -->
+				<thead>
+					<tr>
+						<th class="text-center" >ID</th>
+						<th class="text-center" >Cliente</th>
+						<th class="text-center" >Correo</th>
+						<th class="text-center" >Teléfono</th>
+						<th class="text-center" >Notas</th>
+					</tr>
+				</thead>
+				<!-- // Table heading END -->
+				
+				<!-- Table body -->
+				<tbody>
+					<?php while($renglon_cliente = mysqli_fetch_assoc($renglones_clientes)){?>
+					<!-- Table row -->
+					<tr onclick="ver_cliente(<?php echo $renglon_cliente['id']?>)">
+			<!--ID-->   <td><?= $renglon_cliente['id']; ?></td>
+			<!--Proy--> <td>
+							<h4>
+								<?php echo $renglon_cliente['nombre']; ?>
+							</h4>
+						</td>
+			<!--desc--> <td>
+							<p class="text-muted"><?= $renglon_cliente['correo']; ?></p>
+						</td>
+			<!--prog--> <td class="center"><?= $renglon_cliente['teléfono']; ?></td>
+			<!--asig--> <td class="center"><?= $renglon_cliente['detalles']; ?></td>
+					</tr>
+					<!-- // Table row END -->
+			<?php } ?>
+				</tbody>
+				<!-- // Table body END -->
+			</table>
+			<!-- // Table END -->
+		</div>
     </div>
-    <ul class="pagination pagination-centered margin-none">
-        <li class="disabled"><a href="#">&laquo;</a></li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">&raquo;</a></li>
-    </ul>
 </div>
+
+<script src="../assets/plugins/tables_datatables/js/jquery.dataTables.min.js?v=v1.0.0-rc1&amp;sv=v0.0.1.2"></script>
+<script src="../assets/plugins/tables_datatables/js/dataTables.bootstrap.min.js"></script>
+
+<script>
+/* Initialize Datatables */
+$('#clientes').dataTable({
+	columnDefs: [ { orderable: false, targets: [4] } ],
+	pageLength: 10,
+	lengthMenu: [[10, 20, 30, -1], [10, 20, 30, 'All']]
+});
+
+/* Add placeholder attribute to the search input */
+$('.dataTables_filter input').attr('placeholder', 'Search');
+
+function ver_cliente(id)
+{
+location.href="index.php?page=cliente&id="+id;
+}
+</script>
